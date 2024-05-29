1e1c255662376ff8c8e15918a0c4a6278348889a
Sai Pulipaka <sai.pulipaka@alaskaair.com>
2024-05-29 13:56:04 -0500

    data wedge test 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [device, setDevice] = useState<string>("");
+  const [keypressoutput, setKeypressoutput] = useState<string>("");
+
   useEffect(() => {
-    console.log(window.navigator);
-    setDevice(window.navigator.userAgent);
-  }, []);
-  return <main className={styles.main}>{device}</main>;
+    function keypressHandler(e: any) {
+      console.log("pressed: " + e.keyCode);
+      if (e.keyCode == 13)
+        //  Enter key from DataWedge
+        setKeypressoutput("enter");
+      else {
+        setKeypressoutput(`${keypressoutput}${e.key}`);
+      }
+    }
+    document.addEventListener("keypress", keypressHandler);
+
+    return () => {
+      document.removeEventListener("keypress", keypressHandler);
+    };
+  }, [keypressoutput]);
+
+  return <main className={styles.main}>{keypressoutput}</main>;
 }
